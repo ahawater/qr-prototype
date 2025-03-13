c2ebba6efd95aa43bc34e2902f051743cf8dac9b
Aly Hawater <ali.hawater@gmail.com>
2025-03-13 14:33:30 +0100

    cleaned up, integrated both upper and lower

--- main.cpp
+++ main.cpp
@@ -5,53 +5,58 @@
 
 namespace py = pybind11;
 
-void processImageWithPython(const cv::Mat& inputImage) {
+enum CameraType {
+    LOWER_CAMERA = 0,
+    UPPER_CAMERA = 1
+};
 
+void processImageWithPython(const cv::Mat& inputImage, const CameraType cameraType) {
+
+    // Convert cv::Mat to numpy array
     auto img = py::array_t<unsigned char>(
         {inputImage.rows, inputImage.cols, inputImage.channels()},
         inputImage.data
     );
-
     std::cout << "converted cv::Mat to numpy array" << std::endl;
 
-    // Import the Python script 'qrcodereader' which contains 'detect_qr'
-    // Import the sys module to manipulate sys.path
-    py::module_ sys = py::module_::import("sys");
-
-    // Add the parent directory to the sys.path
+    //Import qrcodereader module
+    py::module_ sys = py::module_::import("sys"); 
     sys.attr("path").attr("append")("../");
-
     py::module_ script = py::module_::import("qrcodereader");
-
     std::cout << "imported qrcodereader" << std::endl;
 
-    // Call the Python 'detect_qr' function
-    py::object result = script.attr("detect_qr")(img);
+    // Call detect_qr function
+    std::cout << "calling detect_qr" << std::endl;
+    py::object result = script.attr("detect_qr")(img, static_cast<int>(cameraType));
+    std::cout << "called detect_qr" << std::endl;
+    py::tuple result_tuple = result.cast<py::tuple>();
 
-    // Check if the result is a tuple (since detect_qr returns multiple values)
-    if (py::isinstance<py::tuple>(result)) {
-        // Cast the result to a tuple
-        py::tuple result_tuple = result.cast<py::tuple>();
-
-        // Extract each element from the tuple
-        float u = result_tuple[0].cast<float>();         // Extract float
-        std::string detected_code = result_tuple[1].cast<std::string>();  // Extract string
-        bool error = result_tuple[2].cast<bool>();       // Extract bool
-
-        // Now, you can use these values in your C++ code
+    // Process the result and print
+    if (cameraType == CameraType::UPPER_CAMERA){
+        bool error = result_tuple[0].cast<bool>();
+        float u = result_tuple[1].cast<float>();
+        std::string detected_code = result_tuple[2].cast<std::string>();
         std::cout << "u: " << u << std::endl;
         std::cout << "Detected QR Code: " << detected_code << std::endl;
         std::cout << "Error: " << (error ? "True" : "False") << std::endl;
-
-    } else {
-        std::cerr << "Expected a tuple, but got a different type!" << std::endl;
+    }
+    else if (cameraType == CameraType::LOWER_CAMERA){
+        std::cout << "Processing upper camera result" << std::endl;
+        bool error = result_tuple[0].cast<bool>();
+        int x = result_tuple[1].cast<float>();
+        int y = result_tuple[2].cast<float>();
+        std::string detected_code = result_tuple[3].cast<std::string>();   
+        std::cout << "x: " << x << std::endl;
+        std::cout << "y: " << y << std::endl;
+        std::cout << "Detected QR Code: " << detected_code << std::endl;
+        std::cout << "Error: " << (error ? "True" : "False") << std::endl;
     }
 
 }
 
 int main() {
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
-    cv::Mat image = cv::imread("../data/upper camera after.jpg");
+    cv::Mat image = cv::imread("../data/v.12 lower camera crop.jpg");
     if (image.empty()) {
         std::cerr << "Error loading image" << std::endl;
         return -1;
@@ -60,7 +65,6 @@ int main() {
 
     py::scoped_interpreter guard{};  // Start Python interpreter
 
-    // Convert cv::Mat to numpy array
     std::cout << "started python interpreter" << std::endl;
 
     py::module_ np = py::module_::import("numpy");
@@ -73,11 +77,11 @@ int main() {
 
 
     for (int i=0; i<5; ++i){
-    auto start = std::chrono::high_resolution_clock::now();
-    processImageWithPython(image);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "Time taken: " << duration.count() << " seconds." << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
+        processImageWithPython(image, CameraType::LOWER_CAMERA);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        std::cout << "Time taken: " << duration.count() << " seconds." << std::endl;
     }
     return 0;
 }
--- qrcodereader.py
+++ qrcodereader.py
@@ -1,54 +1,60 @@
 import cv2
 from qreader import QReader
-import time
 from datetime import datetime
 from math import sqrt
 import os
 
 # Initialize qreader detector
 qr_detector = QReader()
+LOWER_CAMERA = 0
+UPPER_CAMERA = 1
 
-def detect_qr(image):
-    lower_camera = False
-    # Detect QR code using qreader
+def detect_qr(image, camera_type: int):
+
     detections = qr_detector.detect_and_decode(image, return_detections=True)
     detected_codes = detections[0]
     information_detected_codes = detections[1]
 
-    # Plot result
-    if len(detected_codes) > 1: #TODO: sometimes the crop will get a qr code underneath, should not get an error but git the one higher up, to be tested specifically
+    if len(detected_codes) == 0:
+        print("WARNING: NO QR CODES DETECTED")
+        error = True
+        return error, 0, 0, "Error" #TODO: maybe change?
+
+    elif len(detected_codes) > 1: #TODO: sometimes the crop will get a qr code underneath, should not get an error but get the one higher up, to be tested specifically
         print("WARNING: MULTIPLE QR CODES DETECTED")
         error = True
-        return 0, 0, "Error", error #TODO: maybe change?
+        return error, 0, 0, "Error" #TODO: maybe change?
+
 
     detected_code = detected_codes[0]
     if detected_code is not None:
         detected_code = detected_code.replace('/', '_')
     else:
         detected_code = "None"
+
     information = information_detected_codes[0]
     confidence = round(information['confidence'], 2)
     error = False
-    if lower_camera:
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    path = f"../output/time {time}, confidence {confidence}, detected_code {detected_code}.png"
+
+    if camera_type == LOWER_CAMERA:
         x, y = information['cxcy']
-        # Draw a point at the center of the detected QR code
-        cv2.circle(image, (int(x), int(y)), 10, (0, 255, 0), -1)  # Green dot
-        #time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #cv2.imwrite(f"output/time {time}, confidence {confidence}, detected_code {detected_code}.png", image)  #log either date, or have a txt file with the date and time of the image
-        #this repo stays private, just upload the important scripts on notion
-        return x, y, detected_code, error #TODO: maybe change?
-    else:
+        cv2.circle(image, (int(x), int(y)), 10, (0, 255, 0), -1) 
+        cv2.imwrite(path, image)
+        return error, x, y, detected_code #TODO: maybe change?
+    elif camera_type == UPPER_CAMERA:
         x1,y1,x2,y2 = information['bbox_xyxy']
+        y2 = y1 #diagonal to upper side (more accurate)
         u = sqrt((x1-x2)**2 + (y1-y2)**2)
         cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cv2.imwrite(path, image)
+        return error, u, detected_code
+    else:
+        raise ValueError(f"Invalid camera type: {camera_type}")
 
-        cv2.imwrite(f"../output/time {time}, confidence {confidence}, detected_code {detected_code}.png", image)  #log either date, or have a txt file with the date and time of the image
-        return u, detected_code, error
-
-
+#OUTDATED CODE: to test inference time
 # time_taken = []
-
 # for i in range (10):
 #     image = cv2.imread("data/v.12 lower camera crop.jpg")
 #     before = time.time()
@@ -60,6 +66,4 @@ def detect_qr(image):
 # for i in range(len(time_taken)):
 #    print(f"Time taken for iteration {i+1}: {time_taken[i]} seconds")
 
-#return value of the function should be (x,y), that is it (for the lower camera)
-#for the upper camera, it should be u (average of both diagonal?)
-#NOTE: shorter inference for smaller images, logging tajes an extra 20ms.
+#NOTE: shorter inference for smaller images, logging takes an extra 20ms.
